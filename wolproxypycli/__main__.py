063c9a71f814e14321f22d80f262d663154c66b2
Fabio Calefato <fabio.calefato@uniba.it>
2024-02-09 17:21:44 +0100

    Add entry point for command line interface

---
+++ wolproxypycli/__main__.py
@@ -0,0 +1,9 @@
+"""The module is the entry point for the command line interface.
+
+It is responsible for running wolproxypycli as a package.
+"""
+
+from wolproxypycli import main
+
+if __name__ == "__main__":
+    main.run()
